0bf5ad2d9b2e89b69b34af8ec5ccead965438635
gaille73 <40401078+gaille73@users.noreply.github.com>
2018-06-19 22:29:07 +0200

    Update Atomic1.java
    Second comment

--- src/com/winterbe/java8/samples/concurrent/Atomic1.java
+++ src/com/winterbe/java8/samples/concurrent/Atomic1.java
@@ -11,6 +11,7 @@ import java.util.stream.IntStream;
 public class Atomic1 {
 
 	// Test Comment
+	// Second comment
     private static final int NUM_INCREMENTS = 1000;
 
     private static AtomicInteger atomicInt = new AtomicInteger(0);
